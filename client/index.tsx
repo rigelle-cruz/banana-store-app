e3f863e359fe557b3241bb8fa2643340f88461b0
Daniel <danielgrabczewski@gmail.com>
2023-06-28 16:41:21 +1200

    setup frontend folders/files

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
-import App from './components/App'
+import App from './components/AppLayout/AppLayout'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(<App />)
