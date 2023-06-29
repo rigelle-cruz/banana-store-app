76a94967561952b2c2d6d0543794cef8d6739ffe
Rigelle <rigellecruz@gmail.com>
2023-06-29 17:21:50 +1200

    complete FounderStory component

--- client/components/FounderStory/FounderStory.tsx
+++ client/components/FounderStory/FounderStory.tsx
@@ -5,7 +5,9 @@ function FounderStory() {
     <div>
       {founderStories.map((founder, index) => (
         <div key={index}>
+          <img src={founder.image} alt={founder.name} />
           <h5>{founder.name}</h5>
+          <h6>{founder.title}</h6>
           <p>{founder.description}</p>
           <p>Favorite Banana: {founder.favouriteBanana}</p>
         </div>
