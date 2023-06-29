e4c61ed22775024d3e3ef6537739c7406c5e660f
Rigelle <rigellecruz@gmail.com>
2023-06-29 13:03:58 +1200

    create navigation bar

--- client/components/AppLayout/AppLayout.tsx
+++ client/components/AppLayout/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Nav from '../Nav/Nav'
 
 function AppLayout() {
   return (
     <div>
       This is a Navbar!
+      <Nav />
       <Outlet />
     </div>
   )
