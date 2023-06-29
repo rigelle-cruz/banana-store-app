830d1a8434dccf5f08ca5a00af463127ea8041dc
Rigelle <rigellecruz@gmail.com>
2023-06-29 17:14:13 +1200

    add to founder stories

--- client/components/FounderStory/founderStories.ts
+++ client/components/FounderStory/founderStories.ts
@@ -1,36 +1,41 @@
 export const founderStories = [
   {
     name: 'Adam',
-    position: '',
+    title: '',
     description: '',
     favouriteBanana: '',
+    image: 'adam.jpg',
   },
   {
     name: 'Daniel',
-    position: '',
+    title: 'Banana Research and Development Scientist',
     description:
       "Since Daniel's early years, he has been captivated by the allure and versatility of bananas. The vibrant yellow of the Cavendish, the exotic charm of the Red Dacca, and the delightful sweetness of the Lady Finger—each variety holds a unique appeal that never fails to fascinate him. This profound fascination laid the foundation for Daniel's entrepreneurial pursuit, driven by an unyielding love for bananas.",
     favouriteBanana: 'Cavendish',
+    image: 'daniel.jpg',
   },
   {
     name: 'Rigelle',
-    position: '',
+    title: 'Banana Scientist',
     description:
       "Rigelle's tenure in the esteemed banana company has been a delightful journey, fueled by an unwavering passion for the fruit. Captivated by the vibrant hues, tantalizing flavors, and versatile applications of bananas, Rigelle is wholeheartedly dedicated to driving innovation and spreading joy through these remarkable fruits.",
-    favouriteBanana: '',
+    favouriteBanana: 'Blue Java',
+    image: 'rigelle.jpg',
   },
   {
     name: 'Quinn',
-    position: '',
+    title: 'Banana Developer and Analyst',
     description:
       "Quinn's adoration for bananas springs from an inherent fascination with their delectable flavor, nutritional benefits, and versatile nature. It is this profound love for the fruit that propels her to contribute her skills and passion to a banana company, driven by an unwavering desire to share the joy, health benefits, and limitless possibilities that bananas offer to individuals worldwide.",
     favouriteBanana: 'Mysore',
+    image: 'quinn.jpg',
   },
   {
     name: 'Leroy',
-    position: 'Banana Curator and Master of Gastronomic ',
+    title: 'Banana Curator and Master of Gastronomic ',
     description:
       'Leroy embarked on an enthralling journey driven by his unquenchable curiosity for bananas. Through extensive research, hands-on experience, and a natural talent for gastronomy, Leroy honed his expertise in exploring the diverse world of bananas, their varieties, flavors, and culinary applications. His unwavering dedication to banana knowledge and ability to elevate this humble fruit captured our attention.',
     favouriteBanana: 'Goldfinger',
+    image: 'leroy.jpg',
   },
 ]
