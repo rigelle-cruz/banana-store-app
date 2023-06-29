ad75f7c38941a5e9fb0c0c6e341fc10ce77a12b0
Rigelle <rigellecruz@gmail.com>
2023-06-29 16:57:12 +1200

    progress on founder stories

--- client/components/FounderStory/founderStories.ts
+++ client/components/FounderStory/founderStories.ts
@@ -1,28 +1,36 @@
 export const founderStories = [
   {
     name: 'Adam',
+    position: '',
     description: '',
     favouriteBanana: '',
   },
   {
     name: 'Daniel',
+    position: '',
     description:
-      'Since my early years, I have been captivated by the allure and versatility of bananas. The vibrant yellow of the Cavendish, the exotic charm of the Red Dacca, and the delightful sweetness of the Lady Finger—each variety holds a unique appeal that never fails to fascinate me. This profound fascination laid the foundation for my entrepreneurial pursuit, driven by an unyielding love for bananas.',
+      "Since Daniel's early years, he has been captivated by the allure and versatility of bananas. The vibrant yellow of the Cavendish, the exotic charm of the Red Dacca, and the delightful sweetness of the Lady Finger—each variety holds a unique appeal that never fails to fascinate him. This profound fascination laid the foundation for Daniel's entrepreneurial pursuit, driven by an unyielding love for bananas.",
     favouriteBanana: 'Cavendish',
   },
   {
     name: 'Rigelle',
-    description: '',
+    position: '',
+    description:
+      "Rigelle's tenure in the esteemed banana company has been a delightful journey, fueled by an unwavering passion for the fruit. Captivated by the vibrant hues, tantalizing flavors, and versatile applications of bananas, Rigelle is wholeheartedly dedicated to driving innovation and spreading joy through these remarkable fruits.",
     favouriteBanana: '',
   },
   {
     name: 'Quinn',
-    description: '',
+    position: '',
+    description:
+      "Quinn's adoration for bananas springs from an inherent fascination with their delectable flavor, nutritional benefits, and versatile nature. It is this profound love for the fruit that propels her to contribute her skills and passion to a banana company, driven by an unwavering desire to share the joy, health benefits, and limitless possibilities that bananas offer to individuals worldwide.",
     favouriteBanana: 'Mysore',
   },
   {
     name: 'Leroy',
-    description: '',
-    favouriteBanana: '',
+    position: 'Banana Curator and Master of Gastronomic ',
+    description:
+      'Leroy embarked on an enthralling journey driven by his unquenchable curiosity for bananas. Through extensive research, hands-on experience, and a natural talent for gastronomy, Leroy honed his expertise in exploring the diverse world of bananas, their varieties, flavors, and culinary applications. His unwavering dedication to banana knowledge and ability to elevate this humble fruit captured our attention.',
+    favouriteBanana: 'Goldfinger',
   },
 ]
