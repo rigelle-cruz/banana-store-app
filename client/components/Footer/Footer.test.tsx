39cfdcff1c4a9d3b015662df19eecaef56e0a138
Rigelle <rigellecruz@gmail.com>
2023-06-29 14:31:01 +1200

    make progress on footer test

---
+++ client/components/Footer/Footer.test.tsx
@@ -0,0 +1,16 @@
+// @vitest-environment jsdom
+import { describe, expect, it, vi } from 'vitest'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Footer from './Footer'
+import { renderComponent } from '../../test-utils'
+
+describe('Footer', () => {
+  it('links take user to correct page', () => {
+
+
+    const { getAllByRole } = renderComponent 
+
+
+  })
+})
