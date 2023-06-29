d1161bf43fb5a236d14069e6eac7f5699df14583
Rigelle <rigellecruz@gmail.com>
2023-06-30 10:48:54 +1200

    passed test if button is on the page

---
+++ client/components/Banners/AboutBanner.test.tsx
@@ -0,0 +1,50 @@
+//@vitest-environment jsdom
+
+import { afterEach, expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { cleanup } from '@testing-library/react'
+import matchers from '@testing-library/jest-dom/matchers'
+import userEvent from '@testing-library/user-event'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+
+import AboutBanner from './AboutBanner'
+import Shop from '../../pages/Shop/Shop'
+import About from '../../pages/About/About'
+
+afterEach(cleanup)
+expect.extend(matchers)
+const user = userEvent.setup()
+
+test('If button is on the page', async () => {
+  render(
+    <MemoryRouter>
+      <AboutBanner />
+    </MemoryRouter>
+  )
+
+  const button = screen.getByRole('button', {
+    name: 'view full range',
+  })
+
+  console.log()
+
+  expect(button).toBeInTheDocument()
+})
+
+// test('If button redirects to shop', async () => {
+//   render(
+//     <MemoryRouter initialEntries={['/about']}>
+//       <AboutBanner />
+
+//       <Routes>
+//         <Route path="/about" element={<About />} />
+//         <Route path="/shop" element={<Shop />} />
+//       </Routes>
+//     </MemoryRouter>
+//   )
+
+//   const button = screen.getByRole('button', { name: 'view full range' })
+//   await user.click(button)
+
+//   expect(window.location.pathname).toBe('/shop')
+// })
