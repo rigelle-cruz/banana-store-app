a9dcce5cff417ffa336df0a6026aeb45a5639592
Daniel <danielgrabczewski@gmail.com>
2023-06-30 09:58:52 +1200

    create basic mocked apis

--- client/apis/cart.ts
+++ client/apis/cart.ts
@@ -1,3 +1,13 @@
 import request from 'superagent'
+import { fakeCart } from './fakeData'
 
 const baseUrl = '/api/v1/cart'
+
+
+export async function getCart(userId : number) {
+  // const response = await request.get(`${baseUrl}/${id}`)
+  // return response.body as any
+
+  //MOCKED RESPONSE
+  return Promise.resolve(fakeCart.filter(cartItem => cartItem.userId === userId))
+}
--- client/apis/shop.ts
+++ client/apis/shop.ts
@@ -1,9 +1,23 @@
 import request from 'superagent'
+import { fakeProducts } from './fakeData'
 
 const baseUrl = '/api/v1/shop'
 
 export async function getAllProductsApi() {
-  const response = await request.get(baseUrl)
+  // const response = await request.get(baseUrl)
+  // return response.body as any
 
-  return response.body as any
+  //MOCKED RESPONSE
+  return Promise.resolve(fakeProducts)
 }
+
+export async function getProductByIdApi(id : number) {
+  // const response = await request.get(`${baseUrl}/${id}`)
+  // return response.body as any
+
+  //MOCKED RESPONSE
+  return Promise.resolve(fakeProducts.find(product => product.id === id))
+}
+
+
+
