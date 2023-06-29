45f63c5ed147ba53ce84b1d33caa9a291074a128
Daniel <danielgrabczewski@gmail.com>
2023-06-30 10:57:46 +1200

    add links to products in shop

--- client/pages/Shop/Shop.tsx
+++ client/pages/Shop/Shop.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useMutation } from 'react-query'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
@@ -12,35 +12,44 @@ interface Products {
   imgSrc: string
 }
 
-
-function Shop () {
-  const { isLoading, data } = useQuery('getSongs', async () => {
+function Shop() {
+  const { isLoading, data } = useQuery('getProducts', async () => {
     return await getAllProductsApi()
   })
 
   //Check if data is undefined.
-  if (data === undefined) {
-    return (
-      <p>Error getting data!</p>
-    )
+  if (isLoading) {
+    return <p>Loading...</p>
   }
 
-const products : Products[] = data
+  if (!data) {
+    return <p>Error loading data!</p>
+  }
+
+  const products: Products[] = data
 
   return (
     <>
-      <h1>Shop.</h1>
-      {
-       products.map(product => (
-        <div key = {product.name}>
-          <img src={product.imgSrc} style={{maxWidth : '400px'}} alt="" />
-          <p style={{color : 'white'}}>{product.name}</p>
-          <p style={{color : 'white'}}>${product.price}</p>
-          </div>
-       )
-       )
-      }
-      
+      {!isLoading && data && (
+        <>
+          <h1>Shop.</h1>
+          {!isLoading &&
+            products &&
+            products.map((product) => (
+              <div key={product.name}>
+                <Link to={`/shop/${product.id}`}>
+                  <img
+                    src={product.imgSrc}
+                    style={{ maxWidth: '400px' }}
+                    alt=""
+                  />
+                  <p style={{ color: 'white' }}>{product.name}</p>
+                </Link>
+                <p style={{ color: 'white' }}>${product.price}</p>
+              </div>
+            ))}
+        </>
+      )}
     </>
   )
 }
