219f0bde739ecca29e2ea21b713c3896253de4fa
Quinn Gibson <quinngiibson@gmail.com>
2023-06-30 10:02:23 +1200

    update product testing

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -9,7 +7,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3',
+      filename: path.join(__dirname, 'dev.sqlite3'),
     },
   },
 
