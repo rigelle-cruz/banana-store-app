a825bfd2dc774b1ffd2bb5a21d1fedeb8cd0ab43
Rigelle <rigellecruz@gmail.com>
2023-06-30 14:43:42 +1200

    complete Homepage

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -1,8 +1,4 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import { useNavigate } from 'react-router-dom'
-import { useMutation } from 'react-query'
-import { useQuery } from 'react-query'
-import { useParams } from 'react-router-dom'
+/* eslint-disable react/no-unescaped-entities */
 import FeaturedBanana from '../../components/FeaturedBanana/FeaturedBanana'
 import HomeBanner from '../../components/Banners/HomeBanner'
 
@@ -10,7 +6,23 @@ function Home() {
   return (
     <>
       <HomeBanner />
-      <p>On Home page</p>
+      <div>
+        <h1>Banana Benefits</h1>
+        <h2>Energizing Snack</h2>
+        <p>
+          Loaded with carbohydrates and natural sugars, bananas provide a quick
+          and sustained energy boost. They also contain potassium, which helps
+          maintain muscle function and electrolyte balance.
+        </p>
+        <h2>Mood Enhancement</h2>
+        <p>
+          Bananas contain tryptophan, a precursor to serotonin, the "happy
+          hormone." Increasing serotonin levels, bananas may help improve mood
+          and combat feelings of depression and stress.
+        </p>
+        <img src="images/yellow-banana.png" alt="banana"></img>
+      </div>
+
       <FeaturedBanana />
     </>
   )
