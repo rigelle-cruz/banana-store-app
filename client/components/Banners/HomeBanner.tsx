be0acbe5f35c575077ad937ec7d928b8fe5bee8d
Rigelle <rigellecruz@gmail.com>
2023-06-30 14:36:26 +1200

    complete HomeBanner

--- client/components/Banners/HomeBanner.tsx
+++ client/components/Banners/HomeBanner.tsx
@@ -1,5 +1,34 @@
+import { useNavigate } from 'react-router-dom'
+
 function HomeBanner() {
-  return <div>hello</div>
+  const navigate = useNavigate()
+  function goTo(link: string) {
+    navigate(link)
+  }
+
+  return (
+    <div className="about-banner">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col col-md-6">
+            <div className="col-full-bleed">
+              <img src="images/leaningbanana.png" alt="team"></img>
+            </div>
+          </div>
+          <div className="col col-md-6 col-vertical-center">
+            <div className="col-constrain">
+              <img src="images/logo.png" alt="logo"></img>
+              <h1>Bnana.</h1>
+              <p className="subtitle about-banner__subtitle">
+                Take a tropical journey with our luscious tropical bananas.
+              </p>
+              <button onClick={() => goTo('/shop')}>view full range</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HomeBanner
