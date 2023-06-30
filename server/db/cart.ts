ca6a05ea7bf4eeb131b71fed8357bb9fd45bea6d
Quinn Gibson <quinngiibson@gmail.com>
2023-06-30 12:19:03 +1200

    get cart/:id tests passing

--- server/db/cart.ts
+++ server/db/cart.ts
@@ -1,13 +1,22 @@
 import connection from './connection'
 
-export function getCartById(id: number, db = connection) {
+export interface Cart {
+  id: number
+  name: string
+  price: number
+  quantity: number
+  weight: number
+}
+
+export async function getCartById(id: number, db = connection) {
   return db('cart')
     .join('products', 'cart.product_id', 'products.id')
-    .where('user_id', id)
+    .where('cart.user_id', id)
     .select(
       'cart.user_id as id',
       'products.name as name',
+      'products.price as price',
       'cart.quantity as quantity',
       'products.weight as weight'
-    )
+    ) as unknown as Cart[]
 }
