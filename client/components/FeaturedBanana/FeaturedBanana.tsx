eaa28f16b841375aa98305214a421ce21ac994c9
Rigelle <rigellecruz@gmail.com>
2023-06-30 15:01:14 +1200

    progress on FeaturedBanana

--- client/components/FeaturedBanana/FeaturedBanana.tsx
+++ client/components/FeaturedBanana/FeaturedBanana.tsx
@@ -1,5 +1,48 @@
+import { Link } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import { getAllProductsApi } from '../../apis/shop'
+import { ShopProduct } from '../../../models/shop'
+
 function FeaturedBanana() {
-  return <div>hello</div>
+  const { isLoading, data } = useQuery('getProducts', async () => {
+    return await getAllProductsApi()
+  })
+
+  //Check if data is undefined.
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (!data) {
+    return <p>Error loading data!</p>
+  }
+
+  const products: ShopProduct[] = data
+
+  return (
+    <>
+      {!isLoading && data && (
+        <>
+          <h2>Best sellers</h2>
+          
+
+          {products.map((product) => (
+            <div key={product.name}>
+              <Link to={`/shop/${product.id}`}>
+                <img
+                  src={product.imgSrc}
+                  style={{ maxWidth: '400px' }}
+                  alt={product.name}
+                />
+                <p style={{ color: 'white' }}>{product.name}</p>
+              </Link>
+              <p style={{ color: 'white' }}>${product.price}</p>
+            </div>
+          ))}
+        </>
+      )}
+    </>
+  )
 }
 
 export default FeaturedBanana
