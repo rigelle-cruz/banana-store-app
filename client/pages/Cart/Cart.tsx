504997ca7a3f5e3db00071d33d7ef404d509d91a
Daniel <danielgrabczewski@gmail.com>
2023-07-02 11:59:27 +1200

    make progress on the cart

--- client/pages/Cart/Cart.tsx
+++ client/pages/Cart/Cart.tsx
@@ -4,12 +4,20 @@ import { useMutation } from 'react-query'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import { getCartApi } from '../../apis/cart'
+import { useState } from 'react'
+import { getProductById } from '../../../server/db/product'
 
 
 function Cart () {
-  const { isLoading, data } = useQuery('getSongs', async () => {
-    return await getCartApi(1)
+  const { isLoading, data } = useQuery('getCart', async () => {
+    const cart = await getCartApi(1)
+    console.log('inside useQuery, here is the cart: ', cart)
+    const productsInCart = cart.map(async item => await getProductById(item.productId))
+    console.log('inside useQuery, here is the productsInCart: ', productsInCart)
+    return cart
   })
+
+  //const [cart, setCart] = useState(data)
 
 console.log(data)
 
@@ -21,3 +29,15 @@ console.log(data)
 }
 
 export default Cart
+
+//Run getCart to get all of the cart.
+//For each item in the cart, run through and put the id into getProductById, and then store each banana into a seperate array in a state.
+//Loop through the cart items, displaying all the details from the matching item in the detailed array. 
+//For the price, take the price of the item and multiply it by the quantity.
+//Create a combined array? Store it in state.
+
+//Store state of total cost of cart, by going through cart items and adding up the total cost.
+//When user clicks on increase/decrease quantity, run updateQuantity function.
+//When user clicks on remove button for an item, run removeFromCartById
+//A button that, when the user clicks will run clearCart.
+//When user clicks on Checkout, clearCart() and redirect to a thank you page.
