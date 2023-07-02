5ec6c729989ca26512db77cbb19a16654b17f495
Rigelle <rigellecruz@gmail.com>
2023-07-02 12:20:52 +1200

    progress on contact page

--- client/pages/Contact/Contact.tsx
+++ client/pages/Contact/Contact.tsx
@@ -3,6 +3,11 @@ import ContactForm from '../../components/Form/ContactForm'
 function Contact() {
   return (
     <>
+      <div>
+        <h1>Contact.</h1>
+        <h2>Bnana.</h2>
+        <p>Level 5/12 Morgan Street, Newmarket, Auckland 1023</p>
+      </div>
       <ContactForm />
     </>
   )
