2cbe7381b87750729cbb863bb16bef84c9a8cd76
Rigelle <rigellecruz@gmail.com>
2023-07-02 16:25:33 +1200

    fix nav test

--- client/components/Nav/Nav.test.tsx
+++ client/components/Nav/Nav.test.tsx
@@ -1,9 +1,10 @@
 // @vitest-environment jsdom
 import { describe, expect, it } from 'vitest'
-import Nav from './Nav'
 import { renderComponent } from '../../test-utils'
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+
+import Nav from './Nav'
 
 describe('Footer Links', () => {
   it('footer has the correct amount of links', () => {
@@ -23,10 +24,7 @@ describe('Footer Links', () => {
     expect(buttons[1]).toHaveTextContent('about')
     expect(buttons[2]).toHaveTextContent('contact')
     expect(buttons[3]).toHaveTextContent('shop')
-    expect(buttons[4]).toContainHTML('img')
-
-    //NEED TO TEST FOR LOGIN / LOGOUT BUTTON
-
-    //NEED TO COMPLETE TEST TO CHECK IF BUTTONS ARE TAKING USER TO CORRECT PAGE
+    expect(buttons[4]).toHaveTextContent('log in')
+    expect(buttons[5]).toContainHTML('img')
   })
 })
