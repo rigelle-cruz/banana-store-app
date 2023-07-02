309e1826b40cb319eb5b73ff3cfba4bfeec997e6
Rigelle <rigellecruz@gmail.com>
2023-07-02 12:10:44 +1200

    made form into a separate component from contact page

---
+++ client/components/Form/ContactForm.tsx
@@ -0,0 +1,91 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+
+interface FormData {
+  fullName: string
+  email: string
+  topic: string
+  message: string
+}
+
+const initialFormData: FormData = {
+  fullName: '',
+  email: '',
+  topic: '',
+  message: '',
+}
+
+function ContactForm() {
+  const [formData, setFormData] = useState<FormData>(initialFormData)
+  const [submitted, setSubmitted] = useState<boolean>(false)
+
+  const handleInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // Perform your form submission logic here
+
+    // Display 'submitted' message
+    setSubmitted(true)
+
+    setFormData(initialFormData)
+  }
+
+  return (
+    <div>
+      {submitted && <div>Submitted!</div>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            name="fullName"
+            placeholder="FULL NAME"
+            value={formData.fullName}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="email"
+            name="email"
+            placeholder="EMAIL"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="text"
+            name="topic"
+            placeholder="TOPIC"
+            value={formData.topic}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          <textarea
+            name="message"
+            placeholder="MESSAGE"
+            value={formData.message}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
